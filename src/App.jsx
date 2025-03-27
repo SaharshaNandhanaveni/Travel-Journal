6c1385b556891d0ba3b29e91bcf7b6174f380522
Nandhanaveni Saharsha <saharshan77@gmail.com>
2025-03-27 19:24:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from './Components/Header';
+import MainSection from './Components/MainSection';
+import Features from './Components/Features';
+import FeaturedJournals from './Components/FeaturedJournals';
+import Footer from './Components/Footer';
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
+import ForgotPassword from './Components/ForgotPassword';
+import InvalidEmailError from './Components/InvalidEmailError';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainSection />
+      <Features />
+      <FeaturedJournals />
+      <Footer />
+      <SignUp />
+      <SignIn />
+      <ForgotPassword />
+      <InvalidEmailError/>
+    </div>
+  );
+}
+
+export default App;
