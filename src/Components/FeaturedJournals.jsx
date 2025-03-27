cf3059c3bf2e6388e4499d747c13657819e1c42b
Nandhanaveni Saharsha <saharshan77@gmail.com>
2025-03-27 22:00:04 +0530

    Update FeaturedJournals.jsx

--- src/Components/FeaturedJournals.jsx
+++ src/Components/FeaturedJournals.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link to handle navigation
 
 const FeaturedJournals = () => {
   return (
@@ -7,28 +6,17 @@ const FeaturedJournals = () => {
       <div className="container text-center">
         <h3 className="mb-4">Featured Travel Journals</h3>
         <div className="row">
-          {/* Greek Island Hopping Link */}
           <div className="col-md-4">
-            <Link to="/journal/greek-island-hopping">
-              <img src="https://via.placeholder.com/300" alt="Greek Island" className="img-fluid" />
-              <h5>Greek Island Hopping</h5>
-            </Link>
+            <img src="https://via.placeholder.com/300" alt="Greek Island" className="img-fluid" />
+            <h5>Greek Island Hopping</h5>
           </div>
-
-          {/* Japan in Spring Link */}
           <div className="col-md-4">
-            <Link to="/journal/japan-in-spring">
-              <img src="https://via.placeholder.com/300" alt="Japan in Spring" className="img-fluid" />
-              <h5>Japan in Spring</h5>
-            </Link>
+            <img src="https://via.placeholder.com/300" alt="Japan in Spring" className="img-fluid" />
+            <h5>Japan in Spring</h5>
           </div>
-
-          {/* Peru Adventure Link */}
           <div className="col-md-4">
-            <Link to="/journal/peru-adventure">
-              <img src="https://via.placeholder.com/300" alt="Peru Adventure" className="img-fluid" />
-              <h5>Peru Adventure</h5>
-            </Link>
+            <img src="https://via.placeholder.com/300" alt="Peru Adventure" className="img-fluid" />
+            <h5>Peru Adventure</h5>
           </div>
         </div>
       </div>
